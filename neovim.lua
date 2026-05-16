5d9468d8cdc468528164dea8315b8fe94f8ea785
nishant <hembramnishant50@gmail.com>
2026-05-16 19:10:20 +0530

    Add files via upload

--- neovim.lua
+++ neovim.lua
@@ -1,44 +1,38 @@
 return {
     {
-        "bjarneo/aether.nvim",
-        name = "aether",
+        "catppuccin/nvim",
+        name = "catppuccin",
         priority = 1000,
         opts = {
-            disable_italics = false,
-            colors = {
-                -- Monotone shades (Catppuccin Macchiato Base)
-                base00 = "#1e2030", -- Default background
-                base01 = "#181926", -- Lighter background (status bars)
-                base02 = "#363a4f", -- Selection background
-                base03 = "#5b6078", -- Comments, invisibles
-                base04 = "#9399b2", -- Dark foreground
-                base05 = "#cad3f5", -- Default foreground (Text)
-                base06 = "#f4dbd6", -- Light foreground
-                base07 = "#b8c0e0", -- Light background
-
-                -- Accent colors (Your Purple + Macchiato Palette)
-                base08 = "#ed8796", -- Variables, errors (Red)
-                base09 = "#f5a97f", -- Integers, constants (Orange)
-                base0A = "#eed49f", -- Classes, types (Yellow)
-                base0B = "#a6da95", -- Strings (Green)
-                base0C = "#8bd5ca", -- Support, regex (Teal)
-                base0D = "#be9aee", -- Functions, keywords (Your Theme Purple)
-                base0E = "#c6a0f6", -- Keywords, storage (Mauve)
-                base0F = "#f0c6c6", -- Special, URLs (Flamingo)
+            flavour = "mocha", -- Using Mocha as the base
+            transparent_background = true, -- Set to true to match transparent terminal aesthetics
+            term_colors = true,
+            color_overrides = {
+                mocha = {
+                    -- Overriding standard Mocha colors with your custom purples
+                    mauve = "#c6a0f6",
+                    blue = "#c6a0f6",
+                    green = "#c6a0f6",
+                    teal = "#c19cf2", 
+                },
+            },
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                notify = true,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
             },
         },
-        config = function(_, opts)
-            require("aether").setup(opts)
-            vim.cmd.colorscheme("aether")
-
-            -- Enable hot reload
-            require("aether.hotreload").setup()
-        end,
     },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "aether",
+            colorscheme = "catppuccin",
         },
     },
 }
